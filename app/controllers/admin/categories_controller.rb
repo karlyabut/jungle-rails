a94ed749d1f1ab143ee5b95dd1d17612c21ea95a
karlyabut <karleisenyabut@gmail.com>
2019-10-20 20:08:59 +0000

    Implemented categories view for admin

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,27 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ENV_ADMIN_USERNAME'], password: ENV['ENV_ADMIN_PASSWORD']
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+end
