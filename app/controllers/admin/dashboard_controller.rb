d04f84b88cff78f061e6919216b869aec1d4221b
karlyabut <karleisenyabut@gmail.com>
2019-10-20 02:11:11 +0000

    Added about page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,4 +2,5 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ENV_ADMIN_USERNAME'], password: ENV['ENV_ADMIN_PASSWORD']
   def show
     @products = Product.order(:category_id).all
+  end
 end
