943f875a5711859d129a7ea20ce8ba04882bd161
karlyabut <karleisenyabut@gmail.com>
2019-10-20 21:37:03 +0000

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-  end
-
-  namespace :admin do 
     resources :categories
   end
 
