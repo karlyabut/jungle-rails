a9b7e83c99af3fb59099ab9bb3f33d7b7c57f2ee
karlyabut <karleisenyabut@gmail.com>
2019-10-22 20:14:06 +0000

    Refactored product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,26 +5,34 @@ RSpec.describe Product, type: :model do
     # validation tests/examples here
     before(:each) do #before each test create category and empty_product/product
       @category = Category.new(name:'AHHHHHHHHH')
-      @product = Product.new(name: 'Some product', price: 50, quantity: 5, category: @category)
+      @product = Product.new()
     end
 
     it 'saves' do
-      @product.save
-      expect(@product[:id]).to be_present #save successful if theres an id
+      @product_save = Product.new(name: 'Some product', price: 50, quantity: 5, category: @category)
+      @product_save.save
+      expect(@product_save[:id]).to be_present #save successful if theres an id
     end
 
     it 'with name' do
-      expect(@product.name).to be_present
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:name]).to include("can't be blank")
     end
 
     it 'with price' do
-      expect(@product.price).to be_present
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:name]).to include("can't be blank")
+      expect(@product.errors.messages[:price]).to include("is not a number")
     end
+
     it 'with quantity' do
-      expect(@product.quantity).to be_present
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:quantity]).to include("can't be blank")
     end
+
     it 'with category' do
-      expect(@product.category).to be_present
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:category]).to include("can't be blank")
     end
   end
 end
